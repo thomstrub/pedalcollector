3a4b663adbd3abdff5c84abd809a51fc295f3cae
Thomas Strub <Thom@Thomass-Air.wavecable.com>
2020-12-09 22:13:49 -0800

    part 2 lab complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('pedals/', views.pedals_index, name='index'),
+    path('pedals/<int:pedal_id>/', views.pedals_detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pedal
 
 # Create your views here.
 
 # Add the Pedal class & list and view function below the imports
-class Pedal:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, brand, description, age):
-    self.name = name
-    self.brand = brand
-    self.description = description
-    self.age = age
-
-pedals = [
-  Pedal('Big Muff', 'Electroharmonix', 'Fuzz, big sound', 3),
-  Pedal('RD-2', 'Boss', 'Looping Pedal', 0),
-  Pedal('Blue Paisley', 'Danelectro', 'Crazy cool sound!', 4)
-]
 
 
 def home(request):
@@ -25,4 +14,9 @@ def about(request):
     return render(request, 'about.html')
 
 def pedals_index(request):
-    return render(request, 'pedals/index.html', { 'pedals': pedals })
+    pedals = Pedal.objects.all()
+    return render(request, 'pedals/index.html', {'pedals' : pedals})
+
+def pedals_detail(request, pedal_id):
+    pedal = Pedal.objects.get(id=pedal_id)
+    return render(request, 'pedals/detail.html', { 'pedal': pedal})

6689a0fdee4bf60543f4e4ab4344b3e6f2ead832
Thomas Strub <Thom@Thomass-MacBook-Air.local>
2020-12-10 17:45:21 -0800

    model adjusted, no migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.urls import reverse
 
+GUITARS = (
+    ('L', 'Gibson Les Paul'),
+    ('T', 'Fender Telecaster'),
+    ('S', 'Fender Stratocaster'),
+    ('R', 'Rickenbacker')
+)
+
+AMPS = (
+    ('M', 'Marshall JCM800'),
+    ('F', 'Fender Twin'),
+    ('V', 'Vox AC-30'),
+    ('O', 'Orange AD30')
+)
+
 # Create your models here.
 class Pedal(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +27,23 @@ class Pedal(models.Model):
     # add absolute url instead of creating a success url in PedalClass view 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pedal_id': self.id})
+
+class Session(models.Model):
+    date = models.DateField()
+    guitar = models.CharField(
+        max_length=1,
+        choices=GUITARS,
+        default=GUITARS[0][1]
+    )
+    amp = models.CharField(
+        max_length=1,
+        choices=AMPS,
+        default=AMPS[0][1]
+    )
+    notes = models.TextField(max_length=250)
+
+    pedal = models.ForeignKey(Pedal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_guitar_display()} on {self.get_amp_display()} on {self.date}"
+    
